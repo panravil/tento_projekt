08621aaa4e411eb9b68a2c32cb9b256155e17c67
akihiro1126 <81140444+akihiro1126@users.noreply.github.com>
2022-01-05 11:43:30 +0900

    add device item

--- html_sk/insert.php
+++ html_sk/insert.php
@@ -30,10 +30,28 @@ $conn = new mysqli($servername, $username, $password, 'tento_php');
     $phone = $_REQUEST['phone'];
     $zip_code = $_REQUEST['zipCode'];
     $receipt_code = $_REQUEST['receiptCode'];
+    $time = date("Y-m-d H:i:s");
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    $country = ip_info($ip, "Country"); 
+    $browser = get_browser_name($_SERVER['HTTP_USER_AGENT']);
+    if (ismobile()){
+        $device = "mobile";
+    } else {
+        $device = "display";
+    }
+     
+
+    
     // Performing insert query execution
     // here our table name is college
-    $sql = "INSERT INTO formdata (firstName,lastName,email,phone,zipCode,receiptCode)  VALUES ('$first_name',
-                '$last_name', '$email' , '$phone' , '$zip_code', '$receipt_code' )";
+    $sql = "INSERT INTO formdata (firstName,lastName,email,phone,zipCode,receiptCode,time,ip,country,browser,device)  VALUES ('$first_name',
+                '$last_name', '$email' , '$phone' , '$zip_code', '$receipt_code', '$time','$ip','$country','$browser', '$device' )";
     if (mysqli_query($conn, $sql)) {
         echo "<h3>data stored in a database successfully."
             . " Please browse your localhost php my admin"
@@ -45,6 +63,114 @@ $conn = new mysqli($servername, $username, $password, 'tento_php');
         echo "ERROR: Hush! Sorry $sql. "
         . mysqli_error($conn);
     }
-    // Close connection
+
     mysqli_close($conn);
+    
+    function ip_info($ip = NULL, $purpose = "location", $deep_detect = TRUE) {
+        $output = NULL;
+        if (filter_var($ip, FILTER_VALIDATE_IP) === FALSE) {
+            $ip = $_SERVER["REMOTE_ADDR"];
+            if ($deep_detect) {
+                if (filter_var(@$_SERVER['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP))
+                    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+                if (filter_var(@$_SERVER['HTTP_CLIENT_IP'], FILTER_VALIDATE_IP))
+                    $ip = $_SERVER['HTTP_CLIENT_IP'];
+            }
+        }
+        $purpose    = str_replace(array("name", "\n", "\t", " ", "-", "_"), NULL, strtolower(trim($purpose)));
+        $support    = array("country", "countrycode", "state", "region", "city", "location", "address");
+        $continents = array(
+            "AF" => "Africa",
+            "AN" => "Antarctica",
+            "AS" => "Asia",
+            "EU" => "Europe",
+            "OC" => "Australia (Oceania)",
+            "NA" => "North America",
+            "SA" => "South America"
+        );
+        if (filter_var($ip, FILTER_VALIDATE_IP) && in_array($purpose, $support)) {
+            $ipdat = @json_decode(file_get_contents("http://www.geoplugin.net/json.gp?ip=" . $ip));
+            if (@strlen(trim($ipdat->geoplugin_countryCode)) == 2) {
+                switch ($purpose) {
+                    case "location":
+                        $output = array(
+                            "city"           => @$ipdat->geoplugin_city,
+                            "state"          => @$ipdat->geoplugin_regionName,
+                            "country"        => @$ipdat->geoplugin_countryName,
+                            "country_code"   => @$ipdat->geoplugin_countryCode,
+                            "continent"      => @$continents[strtoupper($ipdat->geoplugin_continentCode)],
+                            "continent_code" => @$ipdat->geoplugin_continentCode
+                        );
+                        break;
+                    case "address":
+                        $address = array($ipdat->geoplugin_countryName);
+                        if (@strlen($ipdat->geoplugin_regionName) >= 1)
+                            $address[] = $ipdat->geoplugin_regionName;
+                        if (@strlen($ipdat->geoplugin_city) >= 1)
+                            $address[] = $ipdat->geoplugin_city;
+                        $output = implode(", ", array_reverse($address));
+                        break;
+                    case "city":
+                        $output = @$ipdat->geoplugin_city;
+                        break;
+                    case "state":
+                        $output = @$ipdat->geoplugin_regionName;
+                        break;
+                    case "region":
+                        $output = @$ipdat->geoplugin_regionName;
+                        break;
+                    case "country":
+                        $output = @$ipdat->geoplugin_countryName;
+                        break;
+                    case "countrycode":
+                        $output = @$ipdat->geoplugin_countryCode;
+                        break;
+                }
+            }
+        }
+        return $output;
+    }
+
+    function get_browser_name($user_agent){
+        $t = strtolower($user_agent);
+        $t = " " . $t;
+        if     (strpos($t, 'opera'     ) || strpos($t, 'opr/')     ) return 'Opera'            ;   
+        elseif (strpos($t, 'edge'      )                           ) return 'Edge'             ;   
+        elseif (strpos($t, 'chrome'    )                           ) return 'Chrome'           ;   
+        elseif (strpos($t, 'safari'    )                           ) return 'Safari'           ;   
+        elseif (strpos($t, 'firefox'   )                           ) return 'Firefox'          ;   
+        elseif (strpos($t, 'msie'      ) || strpos($t, 'trident/7')) return 'Internet Explorer';
+        return 'Unkown';
+    }
+
+    function ismobile() {
+        $is_mobile = '0';
+ 
+        if (preg_match('/(android|up.browser|up.link|mmp|symbian|smartphone|midp|wap|phone)/i', strtolower($_SERVER['HTTP_USER_AGENT']))) {
+           $is_mobile = 1;
+        }
+ 
+        if ((strpos(strtolower($_SERVER['HTTP_ACCEPT']), 'application/vnd.wap.xhtml+xml') > 0) or((isset($_SERVER['HTTP_X_WAP_PROFILE']) or isset($_SERVER['HTTP_PROFILE'])))) {
+           $is_mobile = 1;
+        }
+ 
+        $mobile_ua = strtolower(substr($_SERVER['HTTP_USER_AGENT'], 0, 4));
+        $mobile_agents = array('w3c ', 'acs-', 'alav', 'alca', 'amoi', 'andr', 'audi', 'avan', 'benq', 'bird', 'blac', 'blaz', 'brew', 'cell', 'cldc', 'cmd-', 'dang', 'doco', 'eric', 'hipt', 'inno', 'ipaq', 'java', 'jigs', 'kddi', 'keji', 'leno', 'lg-c', 'lg-d', 'lg-g', 'lge-', 'maui', 'maxo', 'midp', 'mits', 'mmef', 'mobi', 'mot-', 'moto', 'mwbp', 'nec-', 'newt', 'noki', 'oper', 'palm', 'pana', 'pant', 'phil', 'play', 'port', 'prox', 'qwap', 'sage', 'sams', 'sany', 'sch-', 'sec-', 'send', 'seri', 'sgh-', 'shar', 'sie-', 'siem', 'smal', 'smar', 'sony', 'sph-', 'symb', 't-mo', 'teli', 'tim-', 'tosh', 'tsm-', 'upg1', 'upsi', 'vk-v', 'voda', 'wap-', 'wapa', 'wapi', 'wapp', 'wapr', 'webc', 'winw', 'winw', 'xda', 'xda-');
+ 
+        if (in_array($mobile_ua, $mobile_agents)) {
+           $is_mobile = 1;
+        }
+ 
+        if (isset($_SERVER['ALL_HTTP'])) {
+           if (strpos(strtolower($_SERVER['ALL_HTTP']), 'OperaMini') > 0) {
+              $is_mobile = 1;
+           }
+        }
+ 
+        if (strpos(strtolower($_SERVER['HTTP_USER_AGENT']), 'windows') > 0) {
+           $is_mobile = 0;
+        }
+ 
+        return $is_mobile;
+     }
 ?>
