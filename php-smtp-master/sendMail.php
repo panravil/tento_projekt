d700bd804c50ff589cb9b57a0f21b1e1cbed143f
akihiro1126 <akihiro.engineer1126@gmail.com>
2022-01-11 18:48:35 +0900

    500 error

---
+++ php-smtp-master/sendMail.php
@@ -0,0 +1,11 @@
+<?php
+    // $mail = new Travis\SMTP(require __DIR__ . '/src/config.php');
+    // use Travis\SMTP;
+    // $mail = new SMTP($config);
+    // $mail->to('akihiro.engineer1126@gmail.com');
+    // $mail->from('ext.kamil.teply@metsagroup.com', 'Paul T.'); // email is required, name is optional
+    // $mail->subject('Hello World');
+    // $mail->body('This is a <b>HTML</b> email.');
+    // $result = $mail->send();
+    echo 123;
+?>
